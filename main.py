75756c08f318611c7b5f4f26db2b9c9990355010
Omar Rivas <orivas@techno-screen.com>
2022-06-04 21:59:27 -0600

    Se agrega decodificación y mostrar mensajes en diferentes formatos.

--- main.py
+++ main.py
@@ -1,32 +1,10 @@
+import tkinter as tk
 from tkinter import *
 
 alfabeto = "abcdefghijklmnopqrstuvwxyz"
 alfabetoMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 longitudAlfabeto = len(alfabeto)
 
-#Función para asignar valores
-def set_text(text, input):
-    input.delete(0,END)
-    input.insert(0,text)
-    return
-
-#funcion para encriptar
-def desencriptar(ventanaMensaje, mensaje, llave, resultado):
-    print(mensaje)
-    textoEncriptado = ""
-    for letra in mensaje:
-        if not letra.isalpha() or letra.lower() == 'ñ':
-            textoEncriptado += letra
-            continue
-        valorLetra = ord(letra)
-        alfabetoAUsar = alfabeto
-        limite = 97
-        if letra.isupper():
-            limite = 65
-            alfabetoAUsar = alfabetoMayusculas
-        posicion = (valorLetra - limite + llave) % longitudAlfabeto
-        textoEncriptado += alfabetoAUsar[posicion]
-    set_text(textoEncriptado, resultado)
 #funcion para convertir texto a ascii
 def textoAscii(texto):
     return ' '.join(list(map(str,map(ord, texto))))
@@ -41,7 +19,33 @@ def textoHexadecimal(texto):
     return texto.hex()
 
 #funcion para encriptar
-def encriptar(mensaje, llave, resultado):
+def desencriptar(mensaje, llave, resultado):
+    print(mensaje)
+    textoDesencriptado = ""
+    for letra in mensaje:
+        if not letra.isalpha() or letra.lower() == 'ñ':
+            textoDesencriptado += letra
+            continue
+        valor_letra = ord(letra)
+        # Suponemos que es minúscula, así que esto comienza en 97(a) y se usará el alfabeto en minúsculas
+        alfabeto_a_usar = alfabeto
+        limite = 97  # Pero si es mayúscula, comienza en 65(A) y se usa en mayúsculas
+        if letra.isupper():
+            limite = 65
+            alfabeto_a_usar = alfabetoMayusculas
+
+        # Rotamos la letra, ahora hacia la izquierda
+        posicion = (valor_letra - limite - llave) % longitudAlfabeto
+
+        # Convertimos el entero resultante a letra y lo concatenamos
+        textoDesencriptado += alfabeto_a_usar[posicion]
+    resultadoASCII.set(textoAscii(textoDesencriptado))
+    resultadoBinario.set(textoBinario(textoDesencriptado))
+    resultadoHexa.set(textoHexadecimal(textoDesencriptado))
+    resultadoChar.set(textoDesencriptado)
+
+#funcion para encriptar
+def encriptar(mensaje, llave, ventana):
     print(mensaje)
     textoEncriptado = ""
     for letra in mensaje:
@@ -56,26 +60,24 @@ def encriptar(mensaje, llave, resultado):
             alfabetoAUsar = alfabetoMayusculas
         posicion = (valorLetra - limite + llave) % longitudAlfabeto
         textoEncriptado += alfabetoAUsar[posicion]
-    set_text(textoEncriptado, resultado)
+    resultadoASCII.set(textoAscii(textoEncriptado))
+    resultadoBinario.set(textoBinario(textoEncriptado))
+    resultadoHexa.set(textoHexadecimal(textoEncriptado))
+    resultadoChar.set(textoEncriptado)
 
 #Función de asignación de opción
-def ejecutarOpcion(opcion, mensaje, llave, resultado):
+def ejecutarOpcion(opcion, mensaje, llave, ventana):
     if opcion == "Encriptar":
-        encriptar(mensaje, llave, resultado)
+        encriptar(mensaje, llave, ventana)
     else:
-        desencriptar(mensaje, llave, resultado)
-    print(textoEncriptado)
-    print(textoAscii(textoEncriptado))
-    print(textoBinario(textoEncriptado))
-    print(textoHexadecimal(textoEncriptado))
-    return textoEncriptado
+        desencriptar(mensaje, llave, ventana)
 
 #funcion para crear ventana de encriptar/desencriptar
 def ventanaMensaje(opcion):
     #Ventana
     ventanaMensaje = Toplevel(ventana)
     ventanaMensaje.title(f"Guia 2 - {opcion}")
-    ventanaMensaje.geometry("500x250")
+    ventanaMensaje.geometry("500x450")
     ventanaMensaje.resizable(0,0)
     ventanaMensaje.protocol("WM_DELETE_WINDOW", disable_event)
     ventanaMensaje.focus_set()
@@ -85,26 +87,41 @@ def ventanaMensaje(opcion):
     Label(ventanaMensaje, text="Ingresa la llave:").place(x=50, y=100)
     mensaje = Entry(ventanaMensaje)
     mensaje.place(x=175, y=50)
-    llave = Entry(ventanaMensaje)
+    llave = Entry(ventanaMensaje, validate="key", validatecommand=(validation, "%S"))
     llave.place(x=175, y=100)
+    Button(ventanaMensaje, text = opcion, command= lambda: ejecutarOpcion(opcion, mensaje.get(), int(llave.get()), ventana)).place(x=50, y=150)
 
     #Resultados
-    resultado = Entry(ventanaMensaje)
-    resultado.place(x=175, y=200)
-    Button(ventanaMensaje, text = opcion, command= lambda: ejecutarOpcion(opcion, mensaje.get(), int(llave.get()), resultado)).place(x=50, y=150)
-    Label(ventanaMensaje, text="Resultado:").place(x=50, y=200)    
+    Label(ventanaMensaje, text="Resultado ASCII:").place(x=50, y=200)
+    txtASCII = Entry(ventanaMensaje, width = 50, state="readonly", textvariable=resultadoASCII).place(x=175, y=200)
 
+    Label(ventanaMensaje, text="Resultado Binario:").place(x=50, y=250)
+    txtBinario = Entry(ventanaMensaje, width = 50, state="readonly", textvariable=resultadoBinario).place(x=175, y=250)    
+
+    Label(ventanaMensaje, text="Resultado Hexadecimal:").place(x=50, y=300)
+    txtHexa = Entry(ventanaMensaje, width = 50, state="readonly", textvariable=resultadoHexa).place(x=175, y=300)
+
+    Label(ventanaMensaje, text="Resultado char:").place(x=50, y=350)
+    txtChar = Entry(ventanaMensaje, width = 50, state="readonly", textvariable=resultadoChar).place(x=175, y=350)
+    
     #Botón Salir
-    Button(ventanaMensaje, text = "Salir", command= lambda: salir(ventanaMensaje)).place(x=400, y=200)
+    Button(ventanaMensaje, text = "Salir", command= lambda: salir(ventanaMensaje)).place(x=400, y=400)
     ventana.withdraw()
 
 #Método para cerrar las ventanas
 def salir(vModal):
+    resultadoASCII.set("")
+    resultadoBinario.set("")
+    resultadoHexa.set("")
+    resultadoChar.set("")
     vModal.withdraw()
     ventana.deiconify()
 
 def disable_event():
     pass
+
+def only_numbers(char):
+    return char.isdigit()
 
 #ventana para el menu
 ventana = Tk()
@@ -112,6 +129,13 @@ ventana.title("Guia 2 - Arquitectura")
 ventana.geometry("350x100")
 ventana.resizable(0,0)
 cabecera = Label(ventana, text = "Método César").pack()
+
+validation = ventana.register(only_numbers)
+
+resultadoASCII = tk.StringVar()
+resultadoBinario = tk.StringVar()
+resultadoHexa = tk.StringVar()
+resultadoChar = tk.StringVar()
 
 #creacion de botones del menu
 opcionEncriptar = Button(ventana, text="Encriptar Mensaje", command= lambda: ventanaMensaje("Encriptar")).place(x = 50, y = 50)
